5f4bbb57bf3e832df8c607c45c1fb94f9f8b782e
Ella <eunseolee90917@gmail.com>
2025-07-05 08:10:28 +0900

    MainView 생성 및 asset 추가

---
+++ Pium/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Pium
+//
+//  Created by Ella's Mac on 7/5/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
