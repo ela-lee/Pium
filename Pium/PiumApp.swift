1515075fea4cc007fbf2a7705e3185b680d7060e
Ela <eunseolee90917@gmail.com>
2025-07-05 07:47:50 +0900

    Initial Commit

---
+++ Pium/PiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiumApp.swift
+//  Pium
+//
+//  Created by Ella's Mac on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
