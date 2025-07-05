9df8aa73e01ca4d623837b83a55a6e54c0f26d0d
Ella <eunseolee90917@gmail.com>
2025-07-05 10:31:00 +0900

    목표 데이터 추가 및 AddGoalView UI 완성하고, 추가하기 버튼 누르면 저장되도록 함

--- Pium/AddGoalView.swift
+++ Pium/AddGoalView.swift
@@ -8,11 +8,73 @@
 import SwiftUI
 
 struct AddGoalView: View {
+    @EnvironmentObject var goalData: GoalData
+    @State private var goalTitle: String = ""
+    @State private var startDate: Date = Date()
+    @State private var endDate: Date = Date()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30) {
+            
+            VStack {
+                Text("목표설정하기")
+                    .foregroundStyle(Color.text01)
+                    .font(.system(size: 20, weight: .bold))
+                
+                TextField("여기에 목표를 적어보세요.", text:$goalTitle)
+                    .textFieldStyle(RoundedTextFieldStyle())
+                    .padding(.horizontal, 20)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(Color.text01)
+            }
+            
+            GoalPeriodPicker(startDate: $startDate, endDate: $endDate)
+
+            Button {
+                let newGoal = Goal(
+                    goalTitle: goalTitle,
+                    startDate: startDate,
+                    endDate: endDate
+                )
+                goalData.activeGoals.append(newGoal)
+            } label: {
+                goalAddButtonStyle
+            }
+            .buttonStyle(.plain)
+            .padding(.top, 50)
+            }
+        }
+}
+
+// MARK: - 목표생성버튼스타일
+private var goalAddButtonStyle: some View {
+    Text("목표생성하기")
+        .font(.title3)
+        .bold()
+        .padding(.vertical,20)
+        .padding(.horizontal, 60)
+        .background(.white)
+        .clipShape(.capsule)
+        .foregroundStyle(Color.accentColor)
+        .shadow(
+            color:Color.accentColor,
+            radius: 2,
+            x: 0,
+            y: 0
+        )
+}
+
+
+
+struct RoundedTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding()
+            .background(Color(red: 0.9569, green: 0.9569, blue: 0.9569))
+            .clipShape(RoundedRectangle(cornerRadius: 12))
     }
 }
 
 #Preview {
     AddGoalView()
+        .environmentObject(GoalData())
 }
--- Pium/PiumApp.swift
+++ Pium/PiumApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct PiumApp: App {
+    @StateObject var goalData = GoalData()
     var body: some Scene {
         WindowGroup {
             MainView()
+                .environmentObject(goalData)
         }
     }
 }
