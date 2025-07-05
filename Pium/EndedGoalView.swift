55c7b1356b18f81bb514f836bc72dac5c863c759
Ella <eunseolee90917@gmail.com>
2025-07-05 09:18:20 +0900

    MainView 탭뷰추가, 목표추가버튼 생성

---
+++ Pium/EndedGoalView.swift
@@ -0,0 +1,18 @@
+//
+//  EndedGoalView.swift
+//  Pium
+//
+//  Created by Ella's Mac on 7/5/25.
+//
+
+import SwiftUI
+
+struct EndedGoalView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    EndedGoalView()
+}
--- Pium/MainView.swift
+++ Pium/MainView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack(alignment:.bottom) {
+                Color(.background)
+                    .ignoresSafeArea(edges: .all)
+                
+                TabView {
+                    Tab("진행중인 목표", systemImage: "list.bullet.clipboard.fill") {
+                        ActiveGoalView()
+                    }
+                    
+                    Tab("지난 목표", systemImage: "checklist.checked") {
+                        EndedGoalView()
+                    }
+                }
+                
+                NavigationLink {
+                    AddGoalView()
+                } label: {
+                    addGoalButton
+                }
+            }
+        }
+    }
+}
+
+// MARK: - 목표추가버튼
+private var addGoalButton: some View {
+    ZStack {
+    
+        Circle()
+            .frame(width: 60, height: 60)
+            .foregroundStyle(Color.accentColor)
+        
+        Image(systemName: "plus")
+            .foregroundStyle(Color.white)
+            .bold()
     }
 }
 
